0ddd06d4e9ac2a6d365541c68c8ef9df76ffe2fd
Nusrat Nuriyev <nuriyevn@gmail.com>
2016-08-31 18:11:06 +0300

    Payout on its way

--- App/Controllers/Payout.php
+++ App/Controllers/Payout.php
@@ -43,4 +43,55 @@ class Payout extends \Core\Controller
         $date_str = date("Y-m-d H:i:s");
         \Core\View::renderTemplate("Payout/getdate.twig", array("date" => $date_str));
     }
+
+
+    public function payoutAction()
+    {
+        \Core\View::renderTemplate("Payout/payout.twig");
+    }
+
+    public function doPayoutAction()
+    {
+        $destination_id = $_REQUEST['destination_id'];
+        $amount = $_REQUEST['amount'];
+        $date_str = date("Y-m-d H:i:s");
+        $description = $_REQUEST['description'] . " " . $date_str;
+        $order_id = $date_str;
+        $method = "card";
+
+        $openpay = \Openpay::getInstance(Config::DB_ID, Config::DB_PRIVATE_KEY);
+
+        $payoutData = array(
+            'method' => $method,
+            'destination_id' => $destination_id,
+            'amount' => $amount,
+            'description' => $description,
+            'order_id' => $order_id
+        );
+
+        $payout = $openpay->payouts->create($payoutData);
+    }
+
+
+    public function getPayoutAction()
+    {
+        $openpay = \Openpay::getInstance(Config::DB_ID, Config::DB_PRIVATE_KEY);
+
+        $creation_gte = $_REQUEST['creation_gte'];
+        $creation_lte = $_REQUEST['creation_lte'];
+        $offset = $_REQUEST['offset'];
+        $limit = $_REQUEST['limit'];
+
+
+        $findData = array(
+            'creation[gte]' => $creation_gte,
+            'creation[lte]' => $creation_lte,
+            'offset' => $offset,
+            'limit' =>  $limit
+        );
+
+        $payoutList = $openpay->payouts->getList($findData);
+    }
+
+
 }
